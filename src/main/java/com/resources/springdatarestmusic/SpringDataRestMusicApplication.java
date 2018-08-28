e2c5782345558d19de4955f0bc7c9b273e51199d
miesek <krzakj@op.pl>
2018-08-28 13:34:27 +0200

    Initial spring data project

---
+++ src/main/java/com/resources/springdatarestmusic/SpringDataRestMusicApplication.java
@@ -0,0 +1,12 @@
+package com.resources.springdatarestmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRestMusicApplication.class, args);
+	}
+}
